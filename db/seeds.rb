9157478e42a13fde51b377eba60c0b298cba1cdc
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-04-21 13:08:10 -0300

    init seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+case Rails.env
+when 'development'
+  Chapter.destroy_all
+  puts "♻️ Destroyed all Tillages"
+  Book.destroy_all
+  puts "♻️ Destroyed all Composts"
+  User.destroy_all
+  puts "♻️ Destroyed all Users"
+
+  puts 'Creating Composts...'
+  file["adubos"].each do |compost|
+    Compost.create! compost.slice("comercial_name", "nitrogen", "potassium", "calcium", "price_reference")
+  end
+  puts '✅ Done Created Composts!'
+
+  puts 'Creating users...'
+  admin = User.new(id: 1,
+                name: "Admin",
+                email: "test@test.com",
+                password: "password",
+                admin: true
+                )
+  admin.save!
+  User.create(id: 2,
+        name: "Gabriel Schiavo",
+        email: "gabriel.schiavo0@gmail.com",
+        password: "password"
+      )
+  puts "✅ Done Creating Users!"
+
+  puts "Creating Books..."
+  5.times do 
+    # Book.create(title)
+  end
+  puts "✅ Done Creating Books"
+
+  puts "Creating Chapters..."
+  25.times do 
+    # Chapter.create()
+  end
+  end
+
+when 'test'
+# test-specific seeds ...
+# (Consider having your tests set up the data they need
+# themselves instead of seeding it here!)
+
+when 'production'
+  puts 'Creating user...'
+  admin = User.new(id: 1,
+                name: "Gabriel Administrador",
+                email: "gabrielschiavo0@gmail.com",
+                password: "p4ssw0rd$"
+                admin: true
+                )
+  admin.save!
+  puts "✅ Done Creating User!"
+end

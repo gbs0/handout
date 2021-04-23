5eed48135da7e534b4247673248e43fbc9def41f
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2021-04-23 11:19:16 -0300

    add search route

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ end
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: 'home#index'
+
+  get '/by_topic' => 'pages#search_by_topic', :as => 'topic_search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
